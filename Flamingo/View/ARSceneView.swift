3767039bb32fd44fff99dd91ff5191380db3fa93
oceanicfish@gmail.com <oceanicfish@gmail.com>
2021-01-19 22:14:58 +0900

    Added USDZModel

--- Flamingo/View/ARSceneView.swift
+++ Flamingo/View/ARSceneView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 import ARKit
 import RealityKit
+import AVKit
 
 struct ARSceneView : View {
+    
+    @State var objectIsPlaced : Bool = false
+    @State var isFirstTime : Bool = true
+    
     init() {
         UINavigationBar.appearance().barTintColor = .clear
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
@@ -18,15 +23,26 @@ struct ARSceneView : View {
     var body: some View {
         NavigationView{
             ZStack {
-                ARViewContainer().edgesIgnoringSafeArea(.all)
+                ARViewContainer(objectIsPlaced: $objectIsPlaced, isFirstTime: $isFirstTime)
+                    .edgesIgnoringSafeArea(.all)
             }
             .navigationBarHidden(true)
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
         }
         .navigationBarTitle("", displayMode: .inline)
+        .onLongPressGesture(minimumDuration: 2, perform: {
+                print("[AR Scene] objectIsPlaced updated.(2)")
+            if !objectIsPlaced {
+                objectIsPlaced = true
+            }
+        })
     }
     
     struct ARViewContainer : UIViewRepresentable {
+        
+        @Binding var objectIsPlaced : Bool
+        @Binding var isFirstTime : Bool
         
         func makeUIView(context: Context) -> ARView {
             
@@ -43,12 +59,6 @@ struct ARSceneView : View {
             }
             
             arView.session.run(config)
-//            
-//            let modelEntity = try!
-//                ModelEntity.loadModel(named: "tv_retro.usdz")
-//            let anchorEntity = AnchorEntity(plane: .any)
-//            anchorEntity.addChild(modelEntity)
-//            arView.scene.addAnchor(anchorEntity)
             
             print("[AR Scene] creating is done.")
             
@@ -59,11 +69,30 @@ struct ARSceneView : View {
             
             print("[AR Scene] updating ui view...")
             
-            let modelEntity = try!
-                ModelEntity.loadModel(named: "tv_retro.usdz")
-            let anchorEntity = AnchorEntity(plane: .any)
-            anchorEntity.addChild(modelEntity)
-            uiView.scene.addAnchor(anchorEntity)
+            if objectIsPlaced && isFirstTime {
+                
+                print("[AR Scene] set up AVPlayer")
+                let asset = AVURLAsset(url: URL(string: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")!)
+                let playerItem = AVPlayerItem(asset: asset)
+                let player = AVPlayer()
+                let videoMaterial = VideoMaterial(avPlayer: player)
+                player.replaceCurrentItem(with: playerItem)
+                
+                let videoPlane = try!
+                    ModelEntity.loadModel(named: "monitor.usdz")
+                videoPlane.scale = SIMD3<Float>(x: 3.0, y: 3.0, z: 3.0)
+                videoPlane.model?.materials[1] = videoMaterial
+                
+//                let videoPlane = ModelEntity(mesh: .generatePlane(width: 0.8, depth: 0.45), materials: [videoMaterial])
+                
+                let anchorEntity = AnchorEntity(plane: .horizontal)
+                anchorEntity.addChild(videoPlane)
+                uiView.scene.addAnchor(anchorEntity)
+                
+                player.play()
+                
+                isFirstTime = false
+            }
             
             print("[AR Scene] updating is done.")
             
